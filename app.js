6f2999a183cc68bb810fa2be499e9e5d0f891f52
chabaking <zaslake@gmail.com>
2023-02-21 19:51:47 +0100

    finalizada v1.0

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const passwords = require('./secrets/passwords');
+
+// Connect to Mongo DB in Atlas service print possible errors to console
+mongoDB = `mongodb+srv://test:${passwords.test}@cluster0-ipmon.mongodb.net/local_library?retryWrites=true`;
+const mongoose = require('mongoose');
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var wikiRouter = require('./routes/wiki');
+var catalogRouter = require('./routes/catalog');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Set middleware for handling logging, JSON data, URL parameters, cookies and static files 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/wiki', wikiRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
